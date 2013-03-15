7ff5576460d282d1ff0cd5406b9107e64446a07c
Baker Bokorney <bbokorney@gmail.com>
2013-03-14 20:40:29 -0400

    First working version.

--- open_tabs.js
+++ open_tabs.js
@@ -1,1 +1,9 @@
-console.log("Can you see this?");
+chrome.extension.sendMessage({message: "save"});
+
+var results = document.getElementsByClassName("r");
+
+for(var i = 0; i < 3; ++i) {
+    window.open(results[i].children[0].getAttribute("href"));
+}
+
+chrome.extension.sendMessage({message: "switch"});
